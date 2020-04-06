d7b354ad21213b4ecd5d523b48b1d538a854f360
Bryan OConnor <bryan.o'connor@zionsbancorp.com>
2020-04-06 15:49:32 -0600

    adds changes to date to make it dynamic

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,11 +4,12 @@ import './Footer.css';
 import '../bootstrap.min.css';
 
 function Footer() {
+
   return(
     <div className="row full-row">
       <div className="col-12">
         <p className="footer">
-          <Link to ="/" className="footLink">bryandoconnor.com | © 2020</Link>
+          <Link to ="/" className="footLink">bryandoconnor.com | © <span>{(new Date().getFullYear())}</span></Link>
         </p>
       </div>
     </div>
